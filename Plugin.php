0b8b0339c72ad9f5d36ce1058e8c8c53e98ed936
SamuNatsu <49141130+SamuNatsu@users.noreply.github.com>
2021-07-10 08:43:24 +0800

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -5,7 +5,7 @@
  * 
  * @package RnRWebLive2D
  * @author Rainiar
- * @version 1.3.0
+ * @version 1.3.1
  * @link https://rainiar.top
  */
 class RnRWebLive2D_Plugin implements Typecho_Plugin_Interface {
@@ -22,12 +22,47 @@ class RnRWebLive2D_Plugin implements Typecho_Plugin_Interface {
     }
 
     public static function header() {
-        echo '<link rel="stylesheet" href="' . Helper::options()->pluginUrl . '/RnRWebLive2D/waifu.min.css"><script type="text/javascript" src="' . Helper::options()->pluginUrl . '/RnRWebLive2D/waifu-tips.min.js"></script><script type="text/javascript" src="' . Helper::options()->pluginUrl . '/RnRWebLive2D/live2d.min.js"></script>';
+        $url = Helper::options()->pluginUrl;
+?>
+<link rel="stylesheet" href="<?php echo $url; ?>/RnRWebLive2D/waifu.min.css">
+<script src="<?php echo $url; ?>/RnRWebLive2D/waifu-tips.min.js"></script>
+<script src="<?php echo $url; ?>/RnRWebLive2D/live2d.min.js"></script>
+<?php
     }
 
     public static function footer() {
         $settings = Helper::options()->plugin('RnRWebLive2D');
-        echo '<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span><span class="fui-chat"></span><span class="fui-eye"></span><span class="fui-user"></span><span class="fui-photo"></span><span class="fui-info-circle"></span><span class="fui-cross"></span></div></div><script defer>live2d_settings["modelAPI"]="' . $settings->apiURL . '";live2d_settings["hitokotoAPI"]="' . $settings->htktAPI . '";live2d_settings["modelId"]=' . $settings->mdID . ';live2d_settings["modelTexturesId"]=' . $settings->txID . ';live2d_settings["modelStorage"]=' . ($settings->mdSt == 1 ? 'true' : 'false') . ';live2d_settings["modelRandMode"]="' . ($settings->mdSw == 1 ? 'rand' : 'switch') . '";live2d_settings["modelTexturesRandMode"]="' . ($settings->txSw == 1 ? 'rand' : 'switch') . '";live2d_settings["homePageUrl"]="' . Helper::options()->rootUrl . '";if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)){console.log("[RnRWebLive2D] Disable on mobile devices.");$(".waifu").hide();}else initModel("' . Helper::options()->pluginUrl . '/RnRWebLive2D/waifu-tips.json");</script>';
+?>
+<div class="waifu">
+    <div class="waifu-tips"></div>
+    <canvas id="live2d" class="live2d"></canvas>
+    <div class="waifu-tool">
+        <span class="fui-home"></span>
+        <span class="fui-chat"></span>
+        <span class="fui-eye"></span>
+        <span class="fui-user"></span>
+        <span class="fui-photo"></span>
+        <span class="fui-info-circle"></span>
+        <span class="fui-cross"></span>
+    </div>
+</div>
+<script defer>
+    live2d_settings["modelAPI"] = "<?php echo $settings->apiURL; ?>";
+    live2d_settings["hitokotoAPI"] = "<?php echo $settings->htktAPI; ?>";
+    live2d_settings["modelId"] = <?php echo $settings->mdID; ?>;
+    live2d_settings["modelTexturesId"] = <?php echo $settings->txID; ?>;
+    live2d_settings["modelStorage"] = <?php echo $settings->mdSt == 1 ? 'true' : 'false'; ?>;
+    live2d_settings["modelRandMode"] = "<?php echo $settings->mdSw == 1 ? 'rand' : 'switch'; ?>";
+    live2d_settings["modelTexturesRandMode"] = "<?php echo $settings->txSw == 1 ? 'rand' : 'switch'; ?>";
+    live2d_settings["homePageUrl"] = "<?php echo Helper::options()->rootUrl; ?>";
+    if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+        console.log("[RnRWebLive2D] Disable on mobile devices.");
+        $(".waifu").hide();
+    }
+    else 
+        initModel("<?php echo Helper::options()->pluginUrl; ?>/RnRWebLive2D/waifu-tips.json");
+</script>
+<?php
     }
     
     /**
@@ -48,7 +83,7 @@ class RnRWebLive2D_Plugin implements Typecho_Plugin_Interface {
      * @return void
      */
     public static function config(Typecho_Widget_Helper_Form $form) {
-        $apiURL = new Typecho_Widget_Helper_Form_Element_Text('apiURL', NULL, NULL, _t('Live2D后端API地址'), _t('我们默认不提供此项，你可以去看看 → https://www.fghrsh.net/post/123.html<br/>该插件使用的后端API是源自这位大大的，你可以自己在你的网站搭建API（推荐放在根目录，否则可能出奇怪的问题）'));
+        $apiURL = new Typecho_Widget_Helper_Form_Element_Text('apiURL', NULL, NULL, _t('Live2D后端API地址'), _t('我们默认不提供此项，你可以去看看 → https://www.fghrsh.net/post/123.html<br/>该插件使用的后端API是源自这位大大的，你可以自己在你的网站搭建API（推荐放在根目录）'));
         $form->addInput($apiURL);
         
         $htktAPI = new Typecho_Widget_Helper_Form_Element_Text('htktAPI', NULL, _t('hitokoto.cn'), _t('一言API地址'), _t('默认值为：hitokoto.cn'));
